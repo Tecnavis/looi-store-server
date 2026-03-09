0ea74b3beea71953deeab033f19132b641c53712
thabumon <52034689+thabumon@users.noreply.github.com>
2026-03-10 02:43:15 +0530

    Update productRoutes.js

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Product = require("../models/Product");
+const Product = require("../models/productModel");
 const upload = require("../middleware/upload"); // your multer/cloudinary middleware
 
 // Create Product
