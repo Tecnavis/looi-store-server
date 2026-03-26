16b0d6333ccce8a75215c695a0305faea780f0bd
thabumon <52034689+thabumon@users.noreply.github.com>
2026-03-26 08:42:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // CORS
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://looi.in',
-    'https://www.looi.in',
-    'https://admin.looi.in',
-    'https://looi-store-admin-beta.vercel.app'
-  ],
+  origin: ['*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
