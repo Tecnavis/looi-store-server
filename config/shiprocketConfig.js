25e169a479f4555aeebf2dd315581d9581d0686b
thabumon <52034689+thabumon@users.noreply.github.com>
2026-04-03 00:10:35 +0530

    Update shiprocketConfig.js

--- config/shiprocketConfig.js
+++ config/shiprocketConfig.js
@@ -3,6 +3,6 @@ module.exports = {
     baseURL:         process.env.SHIPROCKET_API_BASE_URL || 'https://apiv2.shiprocket.in/v1/external',
     email:           process.env.SHIPROCKET_EMAIL,
     password:        process.env.SHIPROCKET_PASSWORD,
-    pickupLocation:  'work',      // ✅ exact name from your Shiprocket account
-    channelId:       5486974,     // ✅ your CUSTOM channel id
+channel_id: shiprocketConfig.channelId,
+pickup_location: 'work',                 
 };
